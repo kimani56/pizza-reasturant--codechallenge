90f4a5a4586bd984f4a5d4e627be651ec33f5140
gift kimani <gift.kimani@students.moringaschool.com>
2023-09-27 14:34:12 +0300

    added swagger

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, make_response,jsonify,request
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
 from models import db, Restaurant, Pizza, RestaurantPizza
+from flask_swagger_ui import get_swaggerui_blueprint
 # from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
+
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'#specifies the URI for the sqlite database
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
